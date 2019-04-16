f2bf1a2f16632fa1a2212c750bf653f31217a79a
Lee Fieldhouse <leebtc1991@gmail.com>
2019-04-16 16:33:05 +0100

    added address to company migration

--- database/migrations/2019_04_15_194810_create_companies_table.php
+++ database/migrations/2019_04_15_194810_create_companies_table.php
@@ -17,6 +17,7 @@ class CreateCompaniesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email');
+            $table->string('address');
             $table->string('logo')->nullable();
             $table->string('website');
             $table->timestamps();
