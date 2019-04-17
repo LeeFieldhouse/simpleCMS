fc8e7722bd6b15c56d9266f4be3b0de6dd5ab7c0
Lee Fieldhouse <leebtc1991@gmail.com>
2019-04-17 21:08:23 +0100

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,11 +1,10 @@
 <nav>
     <div class="nav-wrapper">
-      <a href="#!" class="brand-logo">Logo</a>
+      <a href="#!" class="brand-logo">SimpleCMS</a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
         @auth
-        <li><a href="sass.html">Companies</a></li>
-        <li><a href="badges.html">Employees</a></li>
+            <li><a href="/home">Home</a></li>
             <li><a href="/logout">Logout</a></li>
         @endauth
       </ul>
@@ -14,8 +13,7 @@
 
   <ul class="sidenav" id="mobile-demo">
     @auth
-        <li><a href="sass.html">Companies</a></li>
-        <li><a href="badges.html">Employees</a></li>
+        <li><a href="/home">Home</a></li>
         <li><a href="/logout">Logout</a></li>
     @endauth
   </ul>
